432396d6566d91be0bb68971fef4443fbfa183d4
Evan Zelkowitz <evan_zelkowitz@comcast.com>
2017-11-01 15:08:22 -0600

    add new line, some comments

--- plugins/experimental/system_stats/system_stats.c
+++ plugins/experimental/system_stats/system_stats.c
@@ -32,3 +32,50 @@
 
 #define PLUGIN_NAME "system_stats"
 #define DEBUG_TAG PLUGIN_NAME
+
+typedef struct {
+    int txn_slot;
+    TSStatPersistence persist_type;
+    TSMutex stat_creation_mutex;
+  } config_t;
+
+  void
+  TSPluginInit(int argc, const char *argv[])
+  {
+    TSPluginRegistrationInfo info;
+    //TSCont pre_remap_cont, post_remap_cont, global_cont;
+    config_t *config;
+  
+    info.plugin_name   = PLUGIN_NAME;
+    info.vendor_name   = "Apache Software Foundation";
+    info.support_email = "dev@trafficserver.apache.org";
+  
+    if (TSPluginRegister(&info) != TS_SUCCESS) {
+      TSError("[system_stats] Plugin registration failed");
+      return;
+    } else {
+      TSDebug(DEBUG_TAG, "Plugin registration succeeded");
+    }
+  
+    config                      = TSmalloc(sizeof(config_t));
+    config->persist_type        = TS_STAT_NON_PERSISTENT;
+    config->stat_creation_mutex = TSMutexCreate();
+  
+    if (argc > 1) {
+        //config options if necessary
+    }
+  
+#if 0
+    TSHttpArgIndexReserve(PLUGIN_NAME, "txn data", &(config->txn_slot));
+  
+    global_cont = TSContCreate(handle_txn_close, NULL);
+    TSContDataSet(global_cont, (void *)config);
+    TSHttpHookAdd(TS_HTTP_TXN_CLOSE_HOOK, global_cont);
+#endif  
+/****
+ * Do an initial poll here of all stats to save
+ * an initial state
+ * ****/
+    TSDebug(DEBUG_TAG, "Init complete");
+  }
+  
