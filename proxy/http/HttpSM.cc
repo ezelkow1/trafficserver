f4bc004b4229ce297e40435a41bfc2be6b11d992
Evan Zelkowitz <evan.zelkowitz@gmail.com>
2021-06-21 18:10:15 -0700

    Revert "Do NOT kill tunnel if it has any consumer besides HT_HTTP_CLIENT (#7641) (#7698)"
    This reverts commit 4e11206fac7ba683c9ac7b44e8b69181008147e0.

--- proxy/http/HttpSM.cc
+++ proxy/http/HttpSM.cc
@@ -839,9 +839,9 @@ HttpSM::state_watch_for_client_abort(int event, void *data)
    * client.
    */
   case VC_EVENT_EOS: {
-    // We got an early EOS. If the tunnal has cache writer, don't kill it for background fill.
+    // We got an early EOS.
     NetVConnection *netvc = ua_txn->get_netvc();
-    if (ua_txn->allow_half_open() || tunnel.has_consumer_besides_client()) {
+    if (ua_txn->allow_half_open()) {
       if (netvc) {
         netvc->do_io_shutdown(IO_SHUTDOWN_READ);
       }
